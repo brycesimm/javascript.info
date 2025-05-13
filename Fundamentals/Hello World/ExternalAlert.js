4b352e1f081a9dc87758a64cbd3a58fd21bd7b65
brycesimm <brycesimmerman@gmail.com>
2025-05-12 22:56:47 -0400

    Add Variables and Data Types exercises, update Fundamentals exercises to include prompts

--- Fundamentals/Hello World/ExternalAlert.js
+++ Fundamentals/Hello World/ExternalAlert.js
@@ -1,1 +1,7 @@
+/*
+Take the solution of the previous task Show an alert. 
+Modify it by extracting the script content into an external file alert.js, 
+residing in the same folder.
+*/
+
 alert( 'I’m JavaScript!' );
