e60d39d9f8f724ba568765f9573152a52ab7fa4c
brycesimm <brycesimmerman@gmail.com>
2025-06-15 23:17:47 -0400

    Add Promisification folder

---
+++ 10 - Promises & Async Await/6 - Promisification/Promisification.js
@@ -0,0 +1,58 @@
+// Promisification is transforming a function that accepts a callback into one that returns a 
+// promise instead. 
+
+function loadScript(src, callback) {
+  let script = document.createElement('script');
+  script.src = src;
+
+  script.onload = () => callback(null, script);
+  script.onerror = () => callback(new Error(`Script load error for ${src}`));
+
+  document.head.append(script);
+}
+
+// usage:
+// loadScript('path/script.js', (err, script) => {...})
+
+// |
+// |
+// V
+
+function loadScriptPromise(src) {
+  return new Promise((resolve, reject) => {
+    loadScript(src, (err, script) => {
+      if (err) reject(err);
+      else resolve(script);
+    });
+  });
+};
+
+// usage:
+// loadScriptPromise('path/script.js').then(...)
+
+// Here we essentially add a wrapper around the original function to return a promise that is fulfilled 
+// by the original function's callback function 
+
+// Here is a helper function that can return a promisified function (assumes a callback with two 
+// parameters):
+function promisify(f) {
+  return function (...args) { // return a wrapper-function (*)
+    return new Promise((resolve, reject) => {
+      function callback(err, result) { // our custom callback for f (**)
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+
+      args.push(callback); // append our custom callback to the end of f arguments
+
+      f.call(this, ...args); // call the original function
+    });
+  };
+}
+
+// usage:
+// let loadScriptPromisified = promisify(loadScript);
+// loadScriptPromise(...).then(...);
